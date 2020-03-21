2ec10409c9e20f5409ef980fbc09b3b0cacba586
yanyang0063 <yan.yang12@outlook.com>
2020-03-21 19:05:52 -0400

    Update LSTM with Lotka-Volterra.py

--- LSTM with Lotka-Volterra.py
+++ LSTM with Lotka-Volterra.py
@@ -10,28 +10,38 @@ from keras.layers import LSTM
 from keras.layers import Dense
 from sklearn.preprocessing import MinMaxScaler
 
+# This program uses LSTM to predict the population of two species that follows Lotka-Volterra equations.
+# a, b, c, d correspond to the parameters of the equations, and the initial population is given by y0.
+
+# -----------------------------parameters-----------------------------
 a = 1.
 b = 0.1
 c = 1.5
 d = 0.75
 
+# time interval
+t = np.linspace(0, 60, 2000)
 
-def dX_dt(X, t=0):
+# initial population
+y0 = [10, 5]
+
+
+def dX_dt(X):
     return np.array([a * X[0] - b * X[0] * X[1], -c * X[1] + d * b * X[0] * X[1]])
 
 
-t = np.linspace(0, 60, 2000)
-y0 = [10, 5]
 data = sp.integrate.odeint(dX_dt, y0, t)
 
 scaler = MinMaxScaler()
 data = scaler.fit_transform(data)
 
+# Use two thirds of the data as training data
 N = data.shape[0] // 3 * 2
 train_data = data[0:N, :]
 
 x_train = TimeseriesGenerator(train_data, train_data, length=8)
 
+# ------------------------LSTM------------------------
 model = Sequential()
 model.add(LSTM(8, input_shape=(8, 2)))
 model.add(Dense(20, activation='sigmoid'))
@@ -41,6 +51,7 @@ model.compile(adam, loss='mse')
 
 model.fit_generator(x_train, epochs=2000)
 
+# ---------------------prediction----------------------
 pred = []
 batch = train_data[-8:, :].reshape((1, 8, 2))
 for i in range(data.shape[0] - N):
